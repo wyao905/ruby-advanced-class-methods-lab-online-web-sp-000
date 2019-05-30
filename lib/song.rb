1ba94689cfc4abc03b882adcceef1861bd63e707
Warren Yao <warrenyca905@gmail.com>
2019-05-30 01:36:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,13 +18,15 @@ class Song
   
   def self.new_by_name(song_name)
     song = self.new
-    song.name = (song_name)
+    song.name = song_name
     return song
   end
   
   def self.create_by_name(song_name)
     song = self.new
-    song.name = 
+    song.name = song_name
+    self.all << song
+    return song
   
   def self.find_by_name(song_name)
     self.all.find{|song| song.name == song_name}
