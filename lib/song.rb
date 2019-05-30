3b8b138295f7b5488f98d761032c0c62330d8e8e
Warren Yao <warrenyca905@gmail.com>
2019-05-30 01:11:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,9 +16,9 @@ class Song
     song
   end
   
-  def self.new_by_name(name)
+  def self.new_by_name(song_name)
     song = self.new
-    return song
+    song.name = (song_name)
   end
   
   def self.find_by_name(song_name)
