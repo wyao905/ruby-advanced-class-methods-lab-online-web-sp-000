8c8e9560406b5cf4335ecec5a1aeb5dd738a8a57
Warren Yao <warrenyca905@gmail.com>
2019-05-30 02:26:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,7 +47,7 @@ class Song
   
   def self.new_from_filename(file_name)
     working = []
-    working = file_name.delete(/z\/).split(" - ")
+    working = file_name.delete(/\z[^]{4}/).split(" - ")
     art_name = working[0]
     song_name = working[1]
     song = self.new
