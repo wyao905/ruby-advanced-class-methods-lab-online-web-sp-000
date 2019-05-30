5c7a2d85189a8d6b831443a7dce74291d5c81825
Warren Yao <warrenyca905@gmail.com>
2019-05-30 01:41:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,5 +34,10 @@ class Song
   end
   
   def self.find_or_create_by_name(song_name)
-    
+    if self.find_by_name(song_name)
+      return self.find_by_name(song_name)
+    else
+      self.create_by_name(song_name)
+    end
+  end
 end
